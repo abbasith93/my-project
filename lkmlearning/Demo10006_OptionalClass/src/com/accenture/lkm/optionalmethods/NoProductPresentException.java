48f92668a763b6e35153faeae66a9855f337d6ec
abbasith93 <112550739+abbasith93@users.noreply.github.com>
2025-07-14 16:40:43 +0530

    Delete lkmlearning directory

--- lkmlearning/Demo10006_OptionalClass/src/com/accenture/lkm/optionalmethods/NoProductPresentException.java
+++
@@ -1,7 +0,0 @@
-package com.accenture.lkm.optionalmethods;
-
-public class NoProductPresentException extends Exception  {
-	public NoProductPresentException() {
-		System.out.println("Product not present");
-	}
-}
